a8c2b490f5b779d1bf2299c8f30924bef18a642a
miralulu <mirai@katachi>
2022-12-02 10:23:50 +0900

    ThingのModelを-aで作成

---
+++ database/migrations/2022_12_01_140836_create_things_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('things', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->varchar('thing');
+            $table->tinyInteger('thing_flag');
+            $table->tinyInteger('good_thing_order');
+            $table->tinyInteger('bad_thing_order');
+            $table->varchar('bad_thing_workaround');
+            $table->tinyInteger('is_deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('things');
+    }
+};
