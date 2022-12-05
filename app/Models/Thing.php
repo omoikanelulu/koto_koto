50d577cdbaa873fee0103e477730c84776ee08fb
miralulu <mirai@katachi>
2022-12-05 11:34:08 +0900

    sassの適用とリレーションの記載を行った

--- app/Models/Thing.php
+++ app/Models/Thing.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Thing extends Model
 {
     use HasFactory;
+
+    protected $guarded = [
+        'id',
+    ];
+
+    /**
+     * リレーション
+     */
+    public function user()
+    {
+        return $this->hasMany(\App\Models\Thing::class);
+    }
 }
