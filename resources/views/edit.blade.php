9baff6422041854891339b742adbf8dc9b10e1ab
miralulu <mirai@katachi>
2022-12-15 14:20:24 +0900

    動かないedit画面

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -7,7 +7,7 @@
         <div class="col-lg-6 offset-lg-3">
             <label for="thing" class="form-label">デキゴト</label>
             <textarea class="form-control @error('thing') is-invalid @enderror" aria-describedby="validate" name="thing"
-                id="thing" cols="30" rows="5" placeholder="デキゴトを入力">{{ old('thing', $thing->thing) }}</textarea>
+                id="thing" cols="30" rows="5">{{ old('thing', $thing->thing) }}</textarea>
             @error('thing')
                 <div class="invalid-feedback" id="validate">
                     {{ $message }}
@@ -18,7 +18,7 @@
         <div class="col-lg-6 offset-lg-3">
             <label for="bad_thing_workaround" class="form-label">タイサク</label>
             <textarea class="form-control @error('bad_thing_workaround') is-invalid @enderror" aria-describedby="validate"
-                name="bad_thing_workaround" id="bad_thing_workaround" cols="30" rows="10" placeholder="タイサクを入力">{{ old('bad_thing_workaround', $thing->bad_thing_workaround) }}</textarea>
+                name="bad_thing_workaround" id="bad_thing_workaround" cols="30" rows="10">{{ old('bad_thing_workaround', $thing->bad_thing_workaround) }}</textarea>
             @error('bad_thing_workaround')
                 <div class="invalid-feedback" id="validate">
                     {{ $message }}
@@ -30,22 +30,26 @@
             <label for="">イイコト順位</label>
             <div class="form-check">
                 <input class="form-check-input @error('good_thing_order') is-invalid @enderror" aria-describedby="validate"
-                    type="radio" name="good_thing_order" id="good_thing_order_0" value="0" checked>
+                    type="radio" name="good_thing_order" id="good_thing_order_0" value="0"
+                    {{ $thing->good_thing_order == 0 ? checked : '' }}>
                 <label class="form-check-label" for="good_thing_order_0">なし</label>
             </div>
             <div class="form-check">
                 <input class="form-check-input @error('good_thing_order') is-invalid @enderror" aria-describedby="validate"
-                    type="radio" name="good_thing_order" id="good_thing_order_1" value="1">
+                    type="radio" name="good_thing_order" id="good_thing_order_1" value="1"
+                    {{ $thing->good_thing_order == 1 ? checked : '' }}>
                 <label class="form-check-label" for="good_thing_order_1">すごくイイ</label>
             </div>
             <div class="form-check">
                 <input class="form-check-input @error('good_thing_order') is-invalid @enderror" aria-describedby="validate"
-                    type="radio" name="good_thing_order" id="good_thing_order_2" value="2">
+                    type="radio" name="good_thing_order" id="good_thing_order_2" value="2"
+                    {{ $thing->good_thing_order == 2 ? checked : '' }}>
                 <label class="form-check-label" for="good_thing_order_2">イイ</label>
             </div>
             <div class="form-check">
                 <input class="form-check-input @error('good_thing_order') is-invalid @enderror" aria-describedby="validate"
-                    type="radio" name="good_thing_order" id="good_thing_order_3" value="3">
+                    type="radio" name="good_thing_order" id="good_thing_order_3" value="3"
+                    {{ $thing->good_thing_order == 3 ? checked : '' }}>
                 <label class="form-check-label" for="good_thing_order_3">ちょっとイイ</label>
             </div>
             @error('good_thing_order')
@@ -60,22 +64,26 @@
             <label for="">ヤナコト順位</label>
             <div class="form-check form-check">
                 <input class="form-check-input @error('bad_thing_order') is-invalid @enderror" type="radio"
-                    name="bad_thing_order" id="bad_thing_order_0" value="0" checked>
+                    name="bad_thing_order" id="bad_thing_order_0" value="0"
+                    {{ $thing->bad_thing_order == 0 ? checked : '' }}>
                 <label class="form-check-label" for="bad_thing_order_0">なし</label>
             </div>
             <div class="form-check form-check">
                 <input class="form-check-input @error('bad_thing_order') is-invalid @enderror" type="radio"
-                    name="bad_thing_order" id="bad_thing_order_1" value="1">
+                    name="bad_thing_order" id="bad_thing_order_1" value="1"
+                    {{ $thing->bad_thing_order == 1 ? checked : '' }}>
                 <label class="form-check-label" for="bad_thing_order_1">すごくイヤ</label>
             </div>
             <div class="form-check form-check">
                 <input class="form-check-input @error('bad_thing_order') is-invalid @enderror" type="radio"
-                    name="bad_thing_order" id="bad_thing_order_2" value="2">
+                    name="bad_thing_order" id="bad_thing_order_2" value="2"
+                    {{ $thing->bad_thing_order == 2 ? checked : '' }}>
                 <label class="form-check-label" for="bad_thing_order_2">イヤ</label>
             </div>
             <div class="form-check form-check">
                 <input class="form-check-input @error('bad_thing_order') is-invalid @enderror" type="radio"
-                    name="bad_thing_order" id="bad_thing_order_3" value="3">
+                    name="bad_thing_order" id="bad_thing_order_3" value="3"
+                    {{ $thing->bad_thing_order == 3 ? checked : '' }}>
                 <label class="form-check-label" for="bad_thing_order_3">ちょっとイヤ</label>
             </div>
             @error('bad_thing_order')
@@ -84,10 +92,10 @@
                     {{ $message }}
                 </div>
             @enderror
-
         </div>
 
         <div class="col-lg-6 offset-lg-3">
+            {{-- <input type="hidden" name="{{ $thing->id }}" value="{{ $thing->id }}"> --}}
             <button type="submit" class="btn btn-sm btn-primary">記録する</button>
             <a href="{{ route('thing.index') }}">
                 <button type="button" class="btn btn-sm btn-secondary">戻る</button>
