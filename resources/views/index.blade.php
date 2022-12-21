6d1c6ed2471c2276ea2e76a833334c37550f3082
miralulu <mirai@katachi>
2022-12-21 10:09:42 +0900

    余計なdivを削除

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -38,48 +38,41 @@
         </div>
     @else
         <div class="row">
-            <div class="col-lg-12">
+            <div class="col-lg-10 offset-lg-1">
                 <table>
                     @foreach ($things as $thing)
-                        <div class="">
-                            <tr>
-                                <th class="vw-100">
-                                    <h4 class="right-bg-line">{{ $thing->registration_date }}</h4>
-                                </th>
-                            </tr>
-                        </div>
-                        <div class="">
-                            <tr>
-                                <td class="thing">{{ $thing->thing }}</td>
-                            </tr>
-                        </div>
+                        <tr>
+                            <th class="vw-100">
+                                <h4 class="right-bg-line">{{ $thing->registration_date }}</h4>
+                            </th>
+                        </tr>
+                        <tr>
+                            <td class="thing">{!! nl2br(e($thing->thing), false) !!}</td>
+                        </tr>
                         @empty(!$thing->bad_thing_workaround)
-                            <div class="">
-                                <tr class="d-flex justify-content-end">
-                                    <td class="bad-thing-workaround p-3 rounded-pill">{{ $thing->bad_thing_workaround }}</td>
-                                </tr>
-                            </div>
-                        @endempty
-                        <div class="">
-                            <tr>
-                                <td class="d-flex justify-content-end">
-                                    <div class="operation-buttons py-3">
-                                        <a href="{{ route('thing.show', $thing) }}" class="text-decoration-none">
-                                            <button type="button" class="btn btn-sm btn-outline-primary">詳細</button>
-                                        </a>
-                                        <a href="{{ route('thing.edit', $thing) }}" class="text-decoration-none">
-                                            <button type="button" class="btn btn-sm btn-outline-warning">修正</button>
-                                        </a>
-                                        <form class="d-inline" action="{{ route('thing.destroy', $thing) }}" method="post">
-                                            @method('DELETE')
-                                            @csrf
-                                            <button type="submit" onclick="return confirm('削除しますか？');"
-                                                class="btn-sm btn btn-outline-danger">削除</button>
-                                        </form>
-                                    </div>
-                                </td>
+                            <tr class="d-flex justify-content-end">
+                                <td class="bad-thing-workaround p-3 rounded-pill">
+                                    {!! nl2br(e($thing->bad_thing_workaround), false) !!}</td>
                             </tr>
-                        </div>
+                        @endempty
+                        <tr>
+                            <td class="d-flex justify-content-end">
+                                <div class="operation-buttons py-3">
+                                    <a href="{{ route('thing.show', $thing) }}" class="text-decoration-none">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">詳細</button>
+                                    </a>
+                                    <a href="{{ route('thing.edit', $thing) }}" class="text-decoration-none">
+                                        <button type="button" class="btn btn-sm btn-outline-warning">修正</button>
+                                    </a>
+                                    <form class="d-inline" action="{{ route('thing.destroy', $thing) }}" method="post">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button type="submit" onclick="return confirm('削除しますか？');"
+                                            class="btn-sm btn btn-outline-danger">削除</button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
                     @endforeach
                 </table>
             </div>
