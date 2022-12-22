7d27ad879e4c88284e55b5c0934b6e39371b1c68
miralulu <mirai@katachi>
2022-12-22 14:24:10 +0900

    softDeleteを使うように変更

--- app/Models/Thing.php
+++ app/Models/Thing.php
@@ -6,15 +6,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Thing extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $guarded = [
         'id',
         'created_at',
         'updated_at',
+        'deleted_at',
     ];
 
     public function indexThing()
