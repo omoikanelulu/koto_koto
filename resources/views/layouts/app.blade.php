298baa69f258ed92f1f42ccfd12d07cbdcab4901
miralulu <mirai@katachi>
2023-01-10 14:16:56 +0900

    user情報のリンクを作成

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,11 +65,26 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    @if (Route::has('user.index'))
+                                        <a class="dropdown-item" href="{{ route('user.index') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('user-edit-form').submit();">
+                                            ユーザ表示
+                                        </a>
+                                    @endif
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
+
+                                    @if (Route::has('user.index'))
+                                        <form id="user-edit-form" action="{{ route('user.index') }}" method="POST"
+                                            class="d-none">
+                                            @csrf
+                                        </form>
+                                    @endif
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
