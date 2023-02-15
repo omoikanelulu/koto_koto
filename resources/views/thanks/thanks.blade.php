cff721c1f06837cf2fbff2aa69140aaf3b8cb53e
miralulu <mirai@katachi>
2023-02-15 14:47:53 +0900

    サンクスページを作成

---
+++ resources/views/thanks/thanks.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div id="app">
+        <main class="py-4">
+            <div class="container">
+                <div class="col-lg-6 offset-lg-3 text-center p-5">
+                    <h3>ご利用ありがとうございました</h3>
+                    <a class="btn btn-primary m-5" href="{{ url('/top') }}">トップページへ</a>
+                </div>
+            </div>
+        </main>
+    </div>
+</body>
+
+</html>
--- resources/views/user/thanks.blade.php
+++
@@ -1,6 +0,0 @@
-<main class="py-4">
-    <div class="container">
-        <h3>ご利用ありがとうございました</h3>
-        <a href="{{ route('thing.index') }}">トップページへ</a>
-    </div>
-</main>
