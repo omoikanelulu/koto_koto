875393c7fab43b82a4f111e7ff4382174969eefe
miralulu <mirai@katachi>
2023-03-22 13:18:36 +0900

    AIでシーダーを作ってみた

--- database/seeders/ThingSeeder.php
+++ database/seeders/ThingSeeder.php
@@ -12,7 +12,7 @@ class ThingSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
+     * 生成したシーダー
      * @return void
      */
     public function run()
@@ -21,14 +21,45 @@ class ThingSeeder extends Seeder
         for ($i = 0; $i < 20; $i++) {
             Thing::create([
                 'user_id' => 1,
-                'thing' => Str::random(50),
+                'thing' => 'これはテスト用のデータです。' . $i,
                 'thing_flag' => rand(0, 3),
                 'good_thing_order' => rand(1, 3),
                 'bad_thing_order' => rand(1, 3),
-                'bad_thing_workaround' => Str::random(200),
+                'bad_thing_workaround' => 'これはテスト用のデータです。' . $i,
                 'is_deleted' => rand(0, 1),
-                'registration_date' => '2022-12-01 11:20',
+                'registration_date' => date('Y:m:d H:i:s', strtotime('-' . $i . ' day')),
             ]);
         }
     }
+
+
+
+
+
+
+
+
+
+
+    /**
+     * Run the database seeds.
+     * 最初に作成したシーダー
+     * @return void
+     */
+    // public function run()
+    // {
+    //     DB::table('things')->truncate(); //2回目実行の際にシーダー情報をクリア
+    //     for ($i = 0; $i < 20; $i++) {
+    //         Thing::create([
+    //             'user_id' => 1,
+    //             'thing' => Str::random(50),
+    //             'thing_flag' => rand(0, 3),
+    //             'good_thing_order' => rand(1, 3),
+    //             'bad_thing_order' => rand(1, 3),
+    //             'bad_thing_workaround' => Str::random(200),
+    //             'is_deleted' => rand(0, 1),
+    //             'registration_date' => '2022-12-01 11:20',
+    //         ]);
+    //     }
+    // }
 }
