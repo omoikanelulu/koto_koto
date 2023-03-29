1b179159cfc0fb0829e42db2df10e1e0cb4d39b8
miralulu <mirai@katachi>
2023-03-29 14:58:34 +0900

    配置場所の変更

---
+++ resources/views/top/top.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="card col-lg-12 px-0">
+            <div class="card-header">
+                <h2>{{ config('app.name', 'Laravel') }}</h2>
+            </div>
+            <div class="card-body">
+                <h4 class="card-title">{{ config('app.name', 'Laravel') }}とは？</h4>
+                <p class="card-text ms-3">
+                    {!! App\consts\AppMessagesConst::INTRODUCTION !!}
+                </p>
+                <h4 class="card-title">目的</h4>
+                <p class="card-text ms-3">
+                    {!! App\consts\AppMessagesConst::PURPOSE !!}
+                </p>
+                <h4 class="card-title">使い方</h4>
+                <ol class="card-text">
+                    {!! App\consts\AppMessagesConst::USAGE !!}
+                </ol>
+                <a class="btn btn-primary" href="{{ route('login') }}">{{ __('Login') }}</a>
+                <a class="btn btn-outline-info" href="{{ route('register') }}">{{ __('Register') }}</a>
+            </div>
+        </div>
+    </div>
+@endsection
