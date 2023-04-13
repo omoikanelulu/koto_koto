7b5d39793968c83538525a6f3f2ac4d37af0f92f
desktop_lulu <omoikane@gmail.com>
2023-04-13 22:02:06 +0900

    30項目作成するように変更、不要部分の削除

--- database/seeders/ThingSeeder.php
+++ database/seeders/ThingSeeder.php
@@ -18,7 +18,7 @@ class ThingSeeder extends Seeder
     public function run()
     {
         DB::table('things')->truncate(); //2回目実行の際にシーダー情報をクリア
-        for ($i = 0; $i < 20; $i++) {
+        for ($i = 0; $i < 30; $i++) {
             Thing::create([
                 'user_id' => 1,
                 'thing' => 'これはテスト用のデータです。' . $i,
@@ -31,35 +31,4 @@ class ThingSeeder extends Seeder
             ]);
         }
     }
-
-
-
-
-
-
-
-
-
-
-    /**
-     * Run the database seeds.
-     * 最初に作成したシーダー
-     * @return void
-     */
-    // public function run()
-    // {
-    //     DB::table('things')->truncate(); //2回目実行の際にシーダー情報をクリア
-    //     for ($i = 0; $i < 20; $i++) {
-    //         Thing::create([
-    //             'user_id' => 1,
-    //             'thing' => Str::random(50),
-    //             'thing_flag' => rand(0, 3),
-    //             'good_thing_order' => rand(1, 3),
-    //             'bad_thing_order' => rand(1, 3),
-    //             'bad_thing_workaround' => Str::random(200),
-    //             'is_deleted' => rand(0, 1),
-    //             'registration_date' => '2022-12-01 11:20',
-    //         ]);
-    //     }
-    // }
 }
