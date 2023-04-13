e335b878539b7de857e043e7bff49dc9ce12f055
miralulu <mirai@katachi>
2023-04-13 13:30:34 +0900

    不要部分の削除

--- routes/web.php
+++ routes/web.php
@@ -16,53 +16,6 @@ use App\Http\Controllers\UserController;
 |
 */
 
-// Auth::routes();
-
-// Route::get('/', function () {
-//     return Auth::check() ? redirect('/thing') : view('top.top');
-// });
-
-// // トップページ
-// Route::get('/top', function () {
-//     // ログイン状態であるかチェックsi,7trueかfalseで処理を分岐させる
-//     return Auth::check() ? redirect('/thing') : view('top.top');
-// });
-
-// // ユーザを削除してサンクスページを出す試み
-// // アクションを指定する時は[]で囲む必要があるっぽい 【例】[コントローラ名::class, アクション名]
-// Route::get('/thanks', function () {
-//     return view('thanks.thanks');
-// });
-
-// // phpinfo()を表示する
-// Route::get('/info', function () {
-//     return phpinfo();
-// });
-
-// // グラフ化する期間を送信するindexページと表示するshowページ
-// Route::get('/graph', [GraphController::class, 'index'])->name('graph.index_chart');
-// Route::post('/graph/show_chart', [GraphController::class, 'show'])->name('graph.show_chart');
-
-// // middleware('auth')をかけるグループ
-// Route::middleware(['auth'])->group(function () {
-//     Route::resource('/user', UserController::class);
-//     Route::resource('/thing', ThingController::class);
-// });
-
-
-
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Auth::routes();
 
 // トップページ
