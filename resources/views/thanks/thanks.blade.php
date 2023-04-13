183276b36c0960bb6500f38270e8ce1a464968d4
miralulu <mirai@katachi>
2023-04-13 14:45:15 +0900

    トップページへのリンクをroute()へ変更

--- resources/views/thanks/thanks.blade.php
+++ resources/views/thanks/thanks.blade.php
@@ -24,7 +24,7 @@
             <div class="container">
                 <div class="col-lg-6 offset-lg-3 text-center p-5">
                     <h3>ご利用ありがとうございました</h3>
-                    <a class="btn btn-primary m-5" href="{{ url('/top') }}">トップページへ</a>
+                    <a class="btn btn-primary m-5" href="{{ route('top') }}">トップページへ</a>
                 </div>
             </div>
         </main>
