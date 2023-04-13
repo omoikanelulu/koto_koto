0ddc954d4e02dd422ffa537524819cdd2eedefc5
miralulu <mirai@katachi>
2023-04-13 14:44:37 +0900

    サンクスページをmiddleware(['auth'])の外に出した

--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,19 @@ Route::get('/', function () {
     return Auth::check() ? redirect('/thing') : view('top.top');
 })->name('top');
 
+// サンクスページを表示する
+Route::get('/thanks', function () {
+    // セッションに$show_thanksがない場合はトップページへリダイレクト
+    if (!session('show_thanks')) {
+        return redirect('/');
+    }
+    // フラグを削除する
+    session()->forget('show_thanks');
+    return view('thanks.thanks');
+});
+
 // ログインしている時だけアクセスできるルートをグループ化
 Route::middleware(['auth'])->group(function () {
-    // サンクスページを出表示する
-    Route::get('/thanks', function () {
-        return view('thanks.thanks');
-    });
 
     // グラフ化する期間を送信するindexページと表示するshowページ
     Route::get('/graph', [GraphController::class, 'index'])->name('graph.index_chart');
