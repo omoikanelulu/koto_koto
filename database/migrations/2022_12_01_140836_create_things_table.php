551bdfbf1372a54a15bbfe3e356159dd80d04237
miralulu <mirai@katachi>
2023-04-19 13:29:18 +0900

    default値を追加

--- database/migrations/2022_12_01_140836_create_things_table.php
+++ database/migrations/2022_12_01_140836_create_things_table.php
@@ -16,13 +16,13 @@ return new class extends Migration
         Schema::create('things', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id')->comment('ユーザのID');
-            $table->string('thing')->comment('デキゴト');
-            $table->tinyInteger('thing_flag')->comment('0:評価なし|1:good|2:bad|3:どちらも');
-            $table->tinyInteger('good_thing_order')->comment('イイコトの順位');
-            $table->tinyInteger('bad_thing_order')->comment('ヤナコトの順位');
-            $table->string('bad_thing_workaround')->comment('回避策');
-            $table->tinyInteger('is_deleted')->comment('削除済みフラグ');
-            $table->dateTime('registration_date')->comment('登録日');
+            $table->string('thing')->default('')->comment('デキゴト');
+            $table->tinyInteger('thing_flag')->default(0)->comment('0:評価なし|1:good|2:bad|3:どちらも');
+            $table->tinyInteger('good_thing_order')->default(0)->comment('イイコトの順位');
+            $table->tinyInteger('bad_thing_order')->default(0)->comment('ヤナコトの順位');
+            $table->string('bad_thing_workaround')->default('')->comment('回避策');
+            $table->tinyInteger('is_deleted')->default(0)->comment('削除済みフラグ');
+            $table->dateTime('registration_date')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('登録日');
             $table->timestamps();
         });
     }
