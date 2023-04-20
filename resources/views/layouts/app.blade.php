a7da3ced9ab0a4f48065eac59c5e280f1def1888
miralulu <mirai@katachi>
2023-04-20 15:13:36 +0900

    検索窓をハンバーガーメニューに入れないように変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,6 +72,10 @@
                         {{ config('app.name', 'Laravel') }}
                     </a>
                 @endif
+                <ul class="navbar-nav me-auto">
+                    @yield('search_month')
+                </ul>
+
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -80,9 +84,6 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-                        @yield('search_month')
-                    </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
