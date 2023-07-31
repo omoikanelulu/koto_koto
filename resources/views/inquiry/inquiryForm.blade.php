c81600212d275d716cc9c9f053e721122164f9d3
omoikanelulu <r.omoikane@gmail.com>
2023-07-31 14:02:01 +0900

    aria-describedbyとidの値を修正

--- resources/views/inquiry/inquiryForm.blade.php
+++ resources/views/inquiry/inquiryForm.blade.php
@@ -6,11 +6,11 @@
 
         <div class="col-lg-6 offset-lg-3">
             <label for="name" class="form-label">氏名</label>
-            <input class="form-control @error('name') is-invalid @enderror" aria-describedby="validate" name="name"
+            <input class="form-control @error('name') is-invalid @enderror" aria-describedby="validateName" name="name"
                 id="name" placeholder="氏名を入力">{{ old('name', $inquiry->name ?? '') }}
             </input>
             @error('name')
-                <div class="invalid-feedback" id="validate">
+                <div class="invalid-feedback" id="validateName">
                     {{ $message }}
                 </div>
             @enderror
@@ -18,11 +18,11 @@
 
         <div class="col-lg-6 offset-lg-3">
             <label for="email" class="form-label">メールアドレス</label>
-            <input class="form-control @error('email') is-invalid @enderror" aria-describedby="validate" name="email"
+            <input class="form-control @error('email') is-invalid @enderror" aria-describedby="validateEmail" name="email"
                 id="email" placeholder="メールアドレスを入力">{{ old('email', $inquiry->email ?? '') }}
             </input>
             @error('email')
-                <div class="invalid-feedback" id="validate">
+                <div class="invalid-feedback" id="validateEmail">
                     {{ $message }}
                 </div>
             @enderror
@@ -30,11 +30,11 @@
 
         <div class="col-lg-6 offset-lg-3">
             <label for="inquiry" class="form-label">お問い合わせ内容</label>
-            <textarea class="form-control @error('inquiry') is-invalid @enderror" aria-describedby="validate" name="inquiry"
+            <textarea class="form-control @error('inquiry') is-invalid @enderror" aria-describedby="validateInquiry" name="inquiry"
                 id="inquiry" cols="30" rows="10" wrap="hard" placeholder="お問い合わせ内容を入力">{{ old('inquiry', $inquiry->inquiry ?? '') }}
             </textarea>
             @error('inquiry')
-                <div class="invalid-feedback" id="validate">
+                <div class="invalid-feedback" id="validateInquiry">
                     {{ $message }}
                 </div>
             @enderror
