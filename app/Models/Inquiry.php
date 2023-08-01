7ddeb96e66725c0c323de4ae3137b97c01c8845f
omoikanelulu <r.omoikane@gmail.com>
2023-08-01 13:59:31 +0900

    モデルプロパティ追加

--- app/Models/Inquiry.php
+++ app/Models/Inquiry.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Inquiry extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    // Inquiryモデルにテーブル名とプライマリキーを設定する
+    protected $table = 'inquiries';
+    protected $primaryKey = 'id';
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
 }
