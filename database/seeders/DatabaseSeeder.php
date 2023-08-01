bb8502f79330f774c64c76ebd583b93c597e1fa1
omoikanelulu <r.omoikane@gmail.com>
2023-08-01 14:02:01 +0900

    InquirySeederを追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call(ThingSeeder::class);
+        $this->call(InquirySeeder::class);
 
     }
 }
