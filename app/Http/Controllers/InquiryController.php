2a13fbbd0a153bdc6c55e9fc2628574df39d8bce
omoikanelulu <r.omoikane@gmail.com>
2023-08-01 14:00:37 +0900

    store()とdestroy()を追加

--- app/Http/Controllers/InquiryController.php
+++ app/Http/Controllers/InquiryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Inquiry;
 use Illuminate\Http\Request;
 
 class InquiryController extends Controller
@@ -11,8 +12,25 @@ class InquiryController extends Controller
         return view('inquiry.inquiryForm');
     }
 
-    public function store()
+    /**
+     * お問い合わせの登録処理
+     *
+     * @return void
+     */
+    public function store(Request $request)
     {
-        //
+        $inquiry = new Inquiry();
+        $inquiry->fill($request->all());
+        $inquiry->save();
+
+        return redirect(route('inquiry'));
+    }
+
+    public function delete($id)
+    {
+        $inquiry = Inquiry::find($id);
+        $inquiry->delete();
+
+        return redirect(route('inquiry'));
     }
 }
