1327d67e4e7742201410137eed855d5b7cb4f643
omoikanelulu <r.omoikane@gmail.com>
2023-08-09 15:27:10 +0900

    お問い合わせ一覧表示のビューファイル

--- resources/views/inquiry/index.blade.php
+++ resources/views/inquiry/index.blade.php
@@ -1,50 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<h2>ここにお問い合わせ一覧を表示する</h2>
-    <form class="row g-3" action="{{ route('inquiry.store') }}" method="post">
-        @csrf
-
-        <div class="col-lg-6 offset-lg-3">
-            <label for="name" class="form-label">氏名</label>
-            <input class="form-control @error('name') is-invalid @enderror" aria-describedby="validateName" name="name"
-                id="name" placeholder="氏名を入力">{{ old('name', $inquiry->name ?? '') }}
-            </input>
-            @error('name')
-                <div class="invalid-feedback" id="validateName">
-                    {{ $message }}
-                </div>
-            @enderror
+    <div class="col-lg-10 offset-lg-1 mb-5">
+        <h2>お問い合わせ一覧</h2>
+    </div>
+    @foreach ($inquiries as $inquiry)
+        <div class="mb-5">
+            <div class="col-lg-10 offset-lg-1">
+                <label>お問い合わせID : {{ $inquiry->id }}</label>
+            </div>
+            <div class="col-lg-10 offset-lg-1">
+                <label>氏名 : {{ $inquiry->name }}</label>
+            </div>
+            <div class="col-lg-10 offset-lg-1">
+                <label>メールアドレス : {{ $inquiry->email }}</label>
+            </div>
+            <div class="col-lg-10 offset-lg-1">
+                <label for="inquiry">お問い合わせ内容 : </label>
+                <textarea class="form-control" name="inquiry" id="inquiry" cols="30" rows="5" wrap="hard" disabled>{{ $inquiry->inquiry }}</textarea>
+            </div>
         </div>
+    @endforeach
 
-        <div class="col-lg-6 offset-lg-3">
-            <label for="email" class="form-label">メールアドレス</label>
-            <input class="form-control @error('email') is-invalid @enderror" aria-describedby="validateEmail" name="email"
-                id="email" placeholder="メールアドレスを入力">{{ old('email', $inquiry->email ?? '') }}
-            </input>
-            @error('email')
-                <div class="invalid-feedback" id="validateEmail">
-                    {{ $message }}
-                </div>
-            @enderror
-        </div>
-
-        <div class="col-lg-6 offset-lg-3">
-            <label for="inquiry" class="form-label">お問い合わせ内容</label>
-            <textarea class="form-control @error('inquiry') is-invalid @enderror" aria-describedby="validateInquiry" name="inquiry"
-                id="inquiry" cols="30" rows="10" wrap="hard" placeholder="お問い合わせ内容を入力">{{ old('inquiry', $inquiry->inquiry ?? '') }}</textarea>
-            @error('inquiry')
-                <div class="invalid-feedback" id="validateInquiry">
-                    {{ $message }}
-                </div>
-            @enderror
-        </div>
-
-        <div class="col-lg-6 offset-lg-3">
-            <button type="submit" class="btn btn-sm btn-primary">送信する</button>
-            <a href="{{ route('thing.index') }}">
-                <button type="button" class="btn btn-sm btn-secondary">戻る</button>
-            </a>
-        </div>
-    </form>
+    {{-- 戻るボタン --}}
+    <div class="col-lg-10 offset-lg-1">
+        <a href="{{ route('thing.index') }}">
+            <button type="button" class="btn btn-sm btn-secondary">戻る</button>
+        </a>
+    </div>
 @endsection
