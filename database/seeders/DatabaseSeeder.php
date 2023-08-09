76296ace10cf97084e5cc81cfdf53a1a13190445
omoikanelulu <r.omoikane@gmail.com>
2023-08-09 11:58:47 +0900

    ThingSeeder::classをコメントアウト

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(ThingSeeder::class);
+
+        // $this->call(ThingSeeder::class);
+
         $this->call(InquirySeeder::class);
 
     }
