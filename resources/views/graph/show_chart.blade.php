28466f6d102056e765a1f8af9595b2eea4ac28e5
omoikanelulu <r.omoikane@gmail.com>
2023-08-29 11:22:41 +0900

    @pushディレクティブを使用、グラフの表示設定を変更

--- resources/views/graph/show_chart.blade.php
+++ resources/views/graph/show_chart.blade.php
@@ -1,22 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <head>
+    @push('head')
         {{-- チャートを作成するchart.jsを読み込む --}}
         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
         <style>
             /* canvas要素の高さを調整 */
             #user_chart {
-                max-height: 70%;
+                /* min-height: 200px; */
+                /* max-width: 100%; */
                 max-width: 100%;
             }
         </style>
-    </head>
+    @endpush
 
-    {{-- グラフの棒の幅を固定したいが、未着手 --}}
-
-    <body>
+    <div>
         <h1>{!! $const['title'] !!}</h1>
 
         {{-- グラフの描画 --}}
@@ -32,45 +30,58 @@
                 data: {
                     labels: labels,
                     datasets: [{
-                            label: 'Good Thing Order',
+                            // イイコトの表示設定
+                            label: '{{ $const['good_thing_order'] }}',
                             data: goodThingOrders,
                             backgroundColor: 'rgba(75, 192, 192, 0.2)',
                             borderColor: 'rgba(75, 192, 192, 1)',
                             borderWidth: 1,
                             stack: 'stack1',
-                            categoryPercentage: 0.5, // カテゴリー全体の幅の割合を指定
-                            barPercentage: 1.0, // 個々のカテゴリー内での棒の幅の割合を指定
+                            categoryPercentage: 1.0, // カテゴリー全体の幅の割合を指定
+                            barPercentage: 0.8, // 個々のカテゴリー内での棒の幅の割合を指定
+                            maxBarThickness: 50, // バーの最大太さを設定
+                            minBarThickness: 20, // バーの最小太さを設定
                         },
                         {
-                            label: 'Bad Thing Order',
+                            // ヤナコトの表示設定
+                            label: '{{ $const['bad_thing_order'] }}',
                             data: badThingOrders,
                             backgroundColor: 'rgba(255, 99, 132, 0.2)',
                             borderColor: 'rgba(255, 99, 132, 1)',
                             borderWidth: 1,
                             stack: 'stack1',
-                            categoryPercentage: 0.5, // カテゴリー全体の幅の割合を指定
-                            barPercentage: 1.0, // 個々のカテゴリー内での棒の幅の割合を指定
+                            categoryPercentage: 1.0, // カテゴリー全体の幅の割合を指定
+                            barPercentage: 0.8, // 個々のカテゴリー内での棒の幅の割合を指定
+                            maxBarThickness: 50, // バーの最大太さを設定
+                            minBarThickness: 20, // バーの最小太さを設定
                         },
                     ],
                 },
                 options: {
                     responsive: true,
                     indexAxis: 'y',
+                    plugins: {
+                        title: {
+                            display: true,
+                            text: 'キモチの推移',
+                            font: {
+                                size: 20,
+                            }
+                        },
+                    },
                     scales: {
                         y: {
                             beginAtZero: true,
                         },
                         x: {
-                            min: -30,
-                            max: 30,
+                            // 推奨値、指定した値を超える場合は、その値が表示されるまで拡大される
+                            suggestedMin: -10,
+                            suggestedMax: 10,
                             beginAtZero: true,
                             grid: {
                                 drawOnChartArea: false,
                                 drawTicks: false,
                                 drawBorder: false,
-                                borderColor: 'rgba(0, 0, 0, 0.1)',
-                                borderWidth: 1,
-                                zeroLineColor: 'rgba(0, 0, 0, 0.1)',
                                 zeroLineWidth: 1,
                             },
                         },
@@ -85,5 +96,5 @@
             </a>
         </div>
 
-    </body>
+    </div>
 @endsection
